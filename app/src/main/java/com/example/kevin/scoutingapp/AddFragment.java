3d4f3ea1807cdd24fbba1312630709832fe4bc7b
Kevin Raneri <kevin.raneri@gmail.com>
2016-12-05 16:41:16 -0500

    Cleaned up addfragment

--- app/src/main/java/com/example/kevin/scoutingapp/AddFragment.java
+++ app/src/main/java/com/example/kevin/scoutingapp/AddFragment.java
@@ -31,12 +31,10 @@ public class AddFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment AddFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static AddFragment newInstance(String param1, String param2) {
+    public static AddFragment newInstance() {
         AddFragment fragment = new AddFragment();
         Bundle args = new Bundle();
         fragment.setArguments(args);
@@ -84,7 +82,6 @@ public class AddFragment extends Fragment {
      * >Communicating with Other Fragments</a> for more information.
      */
     public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+
     }
 }
---
+++ app/src/main/java/com/example/kevin/scoutingapp/Globals.java
@@ -0,0 +1,7 @@
+package com.example.kevin.scoutingapp;
+
+/**
+ * Created by Kevin on 12/5/2016.
+ */
+public class Globals {
+}
